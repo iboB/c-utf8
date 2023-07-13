7c1b56cf9dce14b21dd26a4a4218fb930c844314
Borislav Stanimirov <b.stanimirov@abv.bg>
2023-07-13 14:12:32 +0300

    c api and tests

---
+++ code/c-utf8/convert.h
@@ -0,0 +1,179 @@
+// Copyright (c) Borislav Stanimirov
+// SPDX-License-Identifier: MIT
+//
+// DOC:
+// int c_utf8_buf_to_utf32_char(uint32_t* out_char32, const char* utf8_buf, const char* utf8_buf_end);
+//      convert utf8 to a 32-bit unicode character
+//      return number of bytes processed from the input stream
+//      errors are silently ignored by skipping forward
+//
+// int c_utf32_char_to_utf8_buf(char* out_utf8_buf, const char* utf8_buf_end, const uint32_t char32);
+//      convert a 32-bit unicode character to utf8 string
+//      return number of bytes written to buf or 0 on error (invalid unicode char, or buf too small)
+//      will not write on utf8_buf_end or beyond
+//
+// typedef struct c_utf8_char_t {
+//     char buf[5];
+//     uint16_t len;
+// } c_utf8_char;
+//
+// int c_utf8_char_to_buf(char* out_utf8_buf, const char* utf8_buf_end, c_utf8_char_t char_utf8);
+//      write a c_utf8_char_t to a buffer
+//      returns number of bytes written on 0 if buffer is not big enough
+//      no bytes will be written if the buffer is not big enough
+//
+// c_utf8_char_t c_utf32_char_to_utf8_char(const uint32_t char32);
+//      convert a 32-bit unicode character to a utf8 character
+//      returns a char with len = 0 on error (invalid unicode char)
+//
+// uint32_t c_utf8_char_to_utf32_char(const c_utf8_char_t char_utf8, int* opt_error);
+//      conver a utf8 character to a 32-but unicode character
+//      optional error argument is provided
+//
+// int c_utf8_buf_to_utf32_char_b(uint32_t* out_char32, const char* utf8_buf, int* opt_error);
+//      branchless convert utf8 to a 32-bit unicode character
+//      ! utf8_buf must point to at least 4 bytes with zeroes after the end of the string
+//      return number of bytes processed from the output stream
+//      optional error argument is provided
+//      regardless of the error argument the function will never return 0
+//      at least 1 byte will always be skipped
+
+#pragma once
+#include <stdint.h>
+#include <assert.h>
+
+#if defined(__cplusplus)
+#   define C_UTF8_EMPTY_VAL {}
+#   if defined C_UTF8_CPP_NAMESPACE
+namespace C_UTF8_CPP_NAMESPACE {
+#   else
+extern "C" {
+#   endif
+#define C_UTF8_INLINE_FUNC inline
+#define C_UTF8_INLINE_VAR inline
+#define C_UTF8_CONST constexpr
+#else
+#define C_UTF8_EMPTY_VAL {0}
+#define C_UTF8_INLINE_FUNC static inline
+#define C_UTF8_INLINE_VAR static
+#define C_UTF8_CONST const
+#endif
+
+C_UTF8_INLINE_VAR C_UTF8_CONST uint32_t C_UTF32_MAX_CODEPOINT = 0x10FFFF;
+C_UTF8_INLINE_VAR C_UTF8_CONST uint32_t C_UTF32_INVALID_CODEPOINT = 0xFFFD;
+
+typedef struct c_utf8_char_t {
+    char buf[5]; // 4 chars + 1 for zero termination to be nice
+    uint16_t len; // uint16 so struct fits in 8 bytes
+} c_utf8_char_t;
+
+// based on https://github.com/skeeto/branchless-utf8 by Christopher Wellons
+C_UTF8_INLINE_FUNC int c_utf8_buf_to_utf32_char_b(uint32_t* out_char32, const char* utf8_buf, int* opt_error) {
+    static C_UTF8_CONST char lengths[] = {
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 3, 3, 4, 0
+    };
+    static C_UTF8_CONST int masks[] = {0x00, 0x7f, 0x1f, 0x0f, 0x07};
+    static C_UTF8_CONST int shiftc[] = {0, 18, 12, 6, 0};
+
+    const uint8_t* const s = (const uint8_t*)utf8_buf;
+    int len = lengths[s[0] >> 3];
+
+    /* Assume a four-byte character and load four bytes. Unused bits are
+     * shifted out.
+     */
+    *out_char32 = (uint32_t)(s[0] & masks[len]) << 18;
+    *out_char32 |= (uint32_t)(s[1] & 0x3f) << 12;
+    *out_char32 |= (uint32_t)(s[2] & 0x3f) << 6;
+    *out_char32 |= (uint32_t)(s[3] & 0x3f) << 0;
+    *out_char32 >>= shiftc[len];
+
+    if (opt_error) {
+        static C_UTF8_CONST uint32_t mins[] = {4194304, 0, 128, 2048, 65536};
+        static C_UTF8_CONST int shifte[] = {0, 6, 4, 2, 0};
+        int* const e = opt_error;
+        const uint32_t c = *out_char32;
+        /* Accumulate the various error conditions. */
+        *e = (c < mins[len]) << 6; // non-canonical encoding
+        *e |= ((c >> 11) == 0x1b) << 7;  // surrogate half?
+        *e |= (c > C_UTF32_MAX_CODEPOINT) << 8;  // out of range?
+        *e |= (s[1] & 0xc0) >> 2;
+        *e |= (s[2] & 0xc0) >> 4;
+        *e |= (s[3]) >> 6;
+        *e ^= 0x2a; // top two bits of each tail byte correct?
+        *e >>= shifte[len];
+    }
+
+    return len + !len;
+}
+
+C_UTF8_INLINE_FUNC uint32_t c_utf8_char_to_utf32_char(const c_utf8_char_t char_utf8, int* opt_error) {
+    uint32_t ret;
+    c_utf8_buf_to_utf32_char_b(&ret, char_utf8.buf, opt_error);
+    return ret;
+}
+
+// Based on stb_to_utf8() from github.com/nothings/stb/
+C_UTF8_INLINE_FUNC c_utf8_char_t c_utf32_char_to_utf8_char(const uint32_t char32) {
+    c_utf8_char_t ret = C_UTF8_EMPTY_VAL;
+    if (char32 < 0x80)
+    {
+        ret.buf[0] = (char)char32;
+        ret.len = 1;
+    }
+    else if (char32 < 0x800)
+    {
+        ret.buf[0] = (char)(0xc0 + (char32 >> 6));
+        ret.buf[1] = (char)(0x80 + (char32 & 0x3f));
+        ret.len = 2;
+    }
+    else if (char32 < 0x10000)
+    {
+        ret.buf[0] = (char)(0xe0 + (char32 >> 12));
+        ret.buf[1] = (char)(0x80 + ((char32 >> 6) & 0x3f));
+        ret.buf[2] = (char)(0x80 + ((char32) & 0x3f));
+        ret.len = 3;
+    }
+    else if (char32 <= 0x10FFFF)
+    {
+        ret.buf[0] = (char)(0xf0 + (char32 >> 18));
+        ret.buf[1] = (char)(0x80 + ((char32 >> 12) & 0x3f));
+        ret.buf[2] = (char)(0x80 + ((char32 >> 6) & 0x3f));
+        ret.buf[3] = (char)(0x80 + ((char32) & 0x3f));
+        ret.len = 4;
+    }
+    return ret;
+}
+
+C_UTF8_INLINE_FUNC int c_utf8_char_to_buf(char* out_utf8_buf, const char* utf8_buf_end, c_utf8_char_t char_utf8) {
+    int size = (int)(utf8_buf_end - out_utf8_buf);
+    if (size < char_utf8.len) return 0;
+    char* psrc = char_utf8.buf;
+    char* ptgt = out_utf8_buf;
+    while (*psrc) { // assume input is valid
+        *ptgt++ = *psrc++;
+    }
+    return char_utf8.len;
+}
+
+C_UTF8_INLINE_FUNC int c_utf8_buf_to_utf32_char(uint32_t* out_char32, const char* utf8_buf, const char* utf8_buf_end) {
+    char s[4] = C_UTF8_EMPTY_VAL;
+    int i = 0;
+    const char* ptr = utf8_buf;
+    while (i < 4 && ptr != utf8_buf_end) {
+        s[i++] = *ptr++;
+    }
+    return c_utf8_buf_to_utf32_char_b(out_char32, s, NULL);
+}
+
+C_UTF8_INLINE_FUNC int c_utf32_char_to_utf8_buf(char* out_utf8_buf, const char* utf8_buf_end, const uint32_t char32) {
+    c_utf8_char_t utf8_char = c_utf32_char_to_utf8_char(char32);
+    return c_utf8_char_to_buf(out_utf8_buf, utf8_buf_end, utf8_char);
+}
+
+#if defined(__cplusplus)
+// dual purpose closing brace
+// if C_UTF8_CPP_NAMESPACE is defined this closes the namespace
+// otherwise it closes extern "C"
+}
+#endif
--- test/t-c-utf8.c
+++ test/t-c-utf8.c
@@ -1,14 +1,114 @@
 // Copyright (c) Borislav Stanimirov
 // SPDX-License-Identifier: MIT
 //
-#include <unity.h>
-
 #include <c-utf8/convert.h>
+
+#include <unity.h>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
 void setUp(void) {}
 void tearDown(void) {}
 
+void test_utf8_char_to_buf(void) {
+    c_utf8_char_t in[5] = {0};
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < i; ++j) {
+            in[i].buf[j] = (char)j + 'a';
+        }
+        in[i].len = (uint16_t)i;
+    }
+    char out[5];
+    const char empty[5] = {0};
+    for (int i = 0; i < 5; ++i) {
+        for (int o = 0; o < 5; ++o) {
+            memset(out, 0, 5);
+            int res = c_utf8_char_to_buf(out, out + o, in[i]);
+            if (i > o) {
+                TEST_ASSERT_EQUAL(0, res);
+                TEST_ASSERT_EQUAL_CHAR_ARRAY(empty, out, 5);
+            }
+            else {
+                TEST_ASSERT_EQUAL(i, res);
+                TEST_ASSERT_EQUAL_STRING(in[i].buf, out);
+            }
+        }
+    }
+}
+
+void test_conversion(const uint32_t c32) {
+    const c_utf8_char_t c8 = c_utf32_char_to_utf8_char(c32);
+
+    int err;
+    uint32_t c32a = c_utf8_char_to_utf32_char(c8, &err);
+    TEST_ASSERT_EQUAL(0, err);
+    TEST_ASSERT_EQUAL(c32, c32a);
+
+    // check convertion to utf8 buf
+    char buf[5] = {0};
+    int len = c_utf32_char_to_utf8_buf(buf, buf + 5, c32);
+    TEST_ASSERT_EQUAL(c8.len, len);
+    TEST_ASSERT_EQUAL_STRING(c8.buf, buf);
+
+    // check error
+    len = c_utf32_char_to_utf8_buf(buf, buf + c8.len - 1, c32);
+    TEST_ASSERT_EQUAL(0, len);
+    TEST_ASSERT_EQUAL_STRING(c8.buf, buf);
+
+    // fill buff with 'x' so that it's not empty
+    for (uint16_t i = c8.len; i < 5; ++i) {
+        buf[i] = 'x';
+    }
+
+    // test min length
+    len = c_utf8_buf_to_utf32_char(&c32a, buf, buf + c8.len);
+    TEST_ASSERT_EQUAL(c32, c32a);
+    TEST_ASSERT_EQUAL(c8.len, len);
+
+    // test past length
+    len = c_utf8_buf_to_utf32_char(&c32a, buf, buf + 5);
+    TEST_ASSERT_EQUAL(c32, c32a);
+    TEST_ASSERT_EQUAL(c8.len, len);
+}
+
+uint32_t rand32(void) {
+    uint32_t ret;
+    ret = rand() & 0xff;
+    ret |= (rand() & 0xff) << 8;
+    ret |= (rand() & 0xff) << 16;
+    ret |= (rand() & 0xff) << 24;
+    return ret;
+}
+
+uint32_t rand32_below(uint32_t max) {
+    return rand32() % max;
+}
+
+void test_all(void) {
+    for (uint32_t c32 = 1; c32 < 126; ++c32) {
+        c_utf8_char_t c8 = c_utf32_char_to_utf8_char(c32);
+        TEST_ASSERT_EQUAL(1, c8.len);
+        TEST_ASSERT_EQUAL(c8.buf[0], c32);
+
+        test_conversion(c32);
+    }
+
+    // fuzz sorta
+    srand((unsigned)time(NULL));
+    int i = 0;
+    while (i < 100) {
+        uint32_t c32 = rand32_below(C_UTF32_MAX_CODEPOINT + 1);
+        if (c32 < 127) continue; // tested above
+        if (c32 >= 0xFFF0 && c32 <= 0xFFFF) continue; // specials
+        test_conversion(c32);
+        ++i;
+    }
+}
+
 int main(void) {
     UNITY_BEGIN();
+    test_utf8_char_to_buf();
+    test_all();
     return UNITY_END();
 }
